7eadeb0ff22f4d312d07b23d07a3ed5f79b1a6b9
Jlon <juuliyakk@gmail.com>
2018-11-10 11:52:35 +0200

    pink grafs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(181, 60);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Список растений\r\n";
+            this.button2.Text = " План выращивания \r\n";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
