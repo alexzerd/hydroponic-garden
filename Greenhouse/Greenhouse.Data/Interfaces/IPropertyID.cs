fa477420370ad7926e4c6e7e275ae2eb734d27f2
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2018-12-12 14:10:54 +0300

    added new interfaces for the simulation

--- Greenhouse/Greenhouse.Data/Interfaces/IPropertyID.cs
+++ Greenhouse/Greenhouse.Data/Interfaces/IPropertyID.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Greenhouse.Data.Interfaces
 {
-    interface IGPAllowedStates
+    interface IPropertyID
     {
-        
     }
 }
--- Greenhouse/Greenhouse.Data/Interfaces/Measurments/IMeasurment.cs
+++ Greenhouse/Greenhouse.Data/Interfaces/Measurments/IMeasurment.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Greenhouse.Data.Interfaces
+namespace Greenhouse.Data.Interfaces.Measurments
 {
-    interface IGPAllowedStates
+    interface IMeasurment
     {
-        
     }
 }
